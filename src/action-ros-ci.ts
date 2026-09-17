b3f65bbbc09f07c3ce3bb9d1d1f30fe96cb58c7b
mosfet80 <10235105+mosfet80@users.noreply.github.com>
2026-09-18 00:09:59 +0200

    Update action-ros-ci.ts
    Signed-off-by: mosfet80 <10235105+mosfet80@users.noreply.github.com>

--- src/action-ros-ci.ts
+++ src/action-ros-ci.ts
@@ -128,8 +128,7 @@ export async function execShellCommand(
 }
 
 //Determine whether all inputs name supported ROS distributions.
-export function validateDistros(ros2Distro: string
-): boolean {
+export function validateDistros(ros2Distro: string): boolean {
 	if (!ros2Distro) {
 		core.setFailed(
 			`Neither '${targetROS2DistroInput}' inputs were set, at least one is required.`,
